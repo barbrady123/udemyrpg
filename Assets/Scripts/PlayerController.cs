5d314a96cb9e67a90d86b2c5c4007e65b8d236a8
David White <dwhite@us.oneilsoft.com>
2023-03-28 07:27:19 -0700

    lesson 26

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
-	
-	public Animator myAnim; 
-	
+
+	public Animator myAnim;
+
 	public static PlayerController instance;
-	
+
 	public string areaTransitionName;
+
+	private Vector3 bottomLeftLimit;
+	private Vector3 topRightLimit;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +27,7 @@ public class PlayerController : MonoBehaviour
 		{
 			Destroy(gameObject);
 		}
-		
+
 		DontDestroyOnLoad(gameObject);
     }
 
@@ -33,9 +36,9 @@ public class PlayerController : MonoBehaviour
     {
 		float rawX = Input.GetAxisRaw("Horizontal");
 		float rawY = Input.GetAxisRaw("Vertical");
-		
+
         theRB.velocity = new Vector2(rawX, rawY) * moveSpeed;
-		
+
 		myAnim.SetFloat("moveX", theRB.velocity.x);
 		myAnim.SetFloat("moveY", theRB.velocity.y);
 
@@ -44,5 +47,17 @@ public class PlayerController : MonoBehaviour
 			myAnim.SetFloat("lastMoveX", rawX);
 			myAnim.SetFloat("lastMoveY", rawY);
 		}
+
+        // keep the player inside the bounds
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x),
+            Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y),
+            transform.position.z);
     }
+
+	public void SetBounds(Vector3 botLeft, Vector3 topRight)
+	{
+		bottomLeftLimit = botLeft + new Vector3(0.5f, 1, 0);
+		topRightLimit = topRight + new Vector3(-0.5f, -1, 0);
+	}
 }
