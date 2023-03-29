a3a91b5633b47b28599b0899f9276536eef45ff5
David White <barbrady123@yahoo.com>
2023-03-28 19:37:42 -0700

    more stuff

---
+++ Assets/Scripts/EssentialsLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssentialsLoader : MonoBehaviour
+{
+    public GameObject UIScreen;
+    public GameObject player;
+
+    // Awake is called when the script is first loaded (prior to Start)
+    private void Awake()
+    {
+        if (UIFade.instance == null)
+        {
+            Instantiate(UIScreen);
+        }
+
+        if (PlayerController.instance == null)
+        {
+            Instantiate(player);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
