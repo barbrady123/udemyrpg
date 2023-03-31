baa4fa1be9cf45da1e15a746ef1bc7d60d1d08d9
David White <barbrady123@yahoo.com>
2023-03-30 19:47:19 -0700

    more work

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public int playerLevel = 1;
+    public int currentXP;
+    public int[] xpToNextLevel;
+    public int maxLevel = 100;
+    public int baseXP = 1000;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+
+    public int strength;
+    public int defense;
+
+    public int weaponPower;
+    public int armorPower;
+
+    public string equippedWeapon;
+    public string equippedArmor;
+
+    public Sprite charImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xpToNextLevel = new int[maxLevel];
+        xpToNextLevel[0] = 0;
+        int lastLevel = 0;
+
+        for (int i = 1; i < maxLevel; i++)
+        {
+            int nextXP = Convert.ToInt32(baseXP * Math.Pow(1.05, i - 1));
+            lastLevel = xpToNextLevel[i] = lastLevel + nextXP;
+        }
+
+        xpToNextLevel[maxLevel] = Int32.MaxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddXP(500);
+        }
+    }
+
+    public void AddXP(int xp)
+    {
+        currentXP += xp;
+
+        int currentLevel = xpToNextLevel.Select((value, index) => new { value, index }).First(x => x.value > currentXP).index;
+
+        while (currentLevel > playerLevel)
+        {
+            playerLevel++;
+
+            maxHP = Convert.ToInt32(maxHP * 1.05f);
+            currentHP = maxHP;
+
+            maxMP += 5;
+            currentMP = maxMP;
+
+            if (playerLevel % 2 == 0)
+            {
+                strength++;
+            }
+            else
+            {
+                defense++;
+            }
+        }
+    }
+}
